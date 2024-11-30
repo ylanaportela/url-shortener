ed6321b23b8027b242d43f3d40136d8f3ab378f5
Ylana Portela <ylanaportela2000@gmail.com>
2024-11-30 10:58:02 -0300

    add SQL queries for inserting and fetching user data

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import helmet from "helmet";
 import dotenv from "dotenv"
+import { database } from "./db";
 
 dotenv.config();
 const app = express();
@@ -12,3 +13,10 @@ const port = process.env.PORT;
 app.listen(port, () => {
   console.log("server ready");
 });
+
+database.query('insert into users(name, email, second_email) values($1, $2, $2)', ['Mohamed', 'test@gmail.com']).then(()=> {
+  database.query("select * from users")
+  .then((data) => {
+    console.log(data.rows)
+  });
+})
