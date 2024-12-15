4ef527999df6c69f4f0d05eea5b60ba710cbe407
Ylana Portela <ylanaportela2000@gmail.com>
2024-12-15 07:55:46 -0300

    add session config middlewares

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,19 @@ import express from "express";
 import helmet from "helmet";
 import dotenv from "dotenv";
 import { urlRoute } from "./url/url.route";
+import { authRoute } from "./auth/auth.routes";
+import { sessionConfig } from "./middleware/session.middleware";
 
 dotenv.config();
-const app = express();
-app.use(helmet());
-app.use(urlRoute);
 
+const app = express();
 const port = process.env.PORT;
+
+app.use(helmet());
+app.use(express.json());
+app.use(sessionConfig);
+app.use(authRoute);
+app.use(urlRoute);
 
 app.listen(port, () => {
   console.log("server ready");
---
+++ src/middleware/auth.middleware.ts
@@ -0,0 +1,14 @@
+import { Request, Response, NextFunction } from "express";
+
+export function authMiddleware (req: Request, res: Response, next: NextFunction) {
+  const userId = req.session.userId;
+
+  if(userId){
+    next();
+    return;
+  }
+  else{
+    res.status(401).send('Unauthorized user');
+    return;
+  }
+};1
